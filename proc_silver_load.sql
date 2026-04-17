92e9da583ac3b70b00c734cb4b9d192e0f32a94d
ChinKF99 <136291127+ChinKF99@users.noreply.github.com>
2026-04-17 16:04:32 +0800

    Add files via upload

--- proc_silver_load.sql
+++ proc_silver_load.sql
@@ -1,14 +1,14 @@
 /*
-===============================================================================
-Stored Procedure: Load Silver Layer (Bronze -> Silver)
-===============================================================================
-Script Purpose:
-    This stored procedure performs the ETL (Extract, Transform, Load) process to 
-    populate the 'silver' schema tables from the 'bronze' schema.
+===========================================================
+Stored Procedure: Load silver layer (Bronze > Silver)
+===========================================================
+Script purpose:
+	This stored procedure performts the ETL (Extract, Transform, Load)
+	process to pupulate the 'silver' schema tables from the 'bronze' schema.
 	Actions Performed:
 		- Truncates Silver tables.
 		- Inserts transformed and cleansed data from Bronze into Silver tables.
-		
+
 Parameters:
     None. 
 	  This stored procedure does not accept any parameters or return any values.
@@ -17,59 +17,200 @@ Usage Example:
     EXEC Silver.load_silver;
 ===============================================================================
 */
+USE Ecommerce
 
 EXEC silver.load_silver
 
 CREATE OR ALTER PROCEDURE silver.load_silver AS
 BEGIN
-	DECLARE @start_time DATETIME, @end_time DATETIME; 
-    BEGIN TRY
-	    PRINT '================================================';
-        PRINT 'LOADING SILVER LAYER';
-        PRINT '================================================';
+	DECLARE @batch_start_time DATETIME, @batch_end_time DATETIME
+	DECLARE @start_time DATETIME, @end_time DATETIME
+	BEGIN TRY
 
-		-- Loading silver.construction
+		PRINT '==============================='
+		PRINT 'LOADING silver.ecommerce'
+		PRINT '==============================='
+
+		-- Loading silver.ecommerce
+		SET @batch_start_time = GETDATE()
 	    SET @start_time = GETDATE();
 
-		PRINT '>>>>>TRUNCATING TABLE: silver.construction';
-		TRUNCATE TABLE silver.construction;
-		PRINT '>>>>>INSERTING TO TABLE: silver.construction';
-		--CTE named cleaned
-		WITH cleaned AS (
-			SELECT
-				project_id,
-				COALESCE(category, 'Unknown') category,
-				COALESCE(contractor, 'Unknown') contractor,
-				TRY_CONVERT(DATE, starting_date, 103) AS starting_date,
-				TRY_CONVERT(DATE, plan_end_date, 103) AS plan_end_date,
-				TRY_CONVERT(DATE, actual_end_date, 103) AS actual_end_date,
-				-- Use extract_numeric function for both budget & actual_cost column
-				CAST(COALESCE(COALESCE(silver.extract_numeric(budget), silver.extract_numeric(actual_cost)),0) AS BIGINT) AS budget,
-				CAST(COALESCE(COALESCE(silver.extract_numeric(actual_cost), silver.extract_numeric(budget)),0) AS BIGINT) AS actual_cost,
-				CAST(COALESCE(delay_days, '0') AS INT) AS delay_days
-			FROM bronze.construction
-		)
-		INSERT INTO silver.construction
-		SELECT 
-		*,
-		(actual_cost - budget) AS cost_overrun_saving,
-		--(actual_cost - budget)/budget AS percentage
-		(CAST(actual_cost AS FLOAT) - budget) / NULLIF(budget, 0) AS cost_overrun_saving_percentage
-		FROM cleaned
-		--Exclude row which both budget & actual_cost column is 0 (Outliers)
-		WHERE NOT (budget = 0 AND actual_cost = 0);
+		PRINT '>>>>>TRUNCATING TABLE: silver.ecommerce';
+		TRUNCATE TABLE silver.ecommerce;
+		PRINT '>>>>>INSERTING TO TABLE: silver.ecommerce';
+
+		-- CTE to esure data cleansing, standardizaiton & consistency
+		WITH cte_cleaned_data AS (
+		SELECT
+			CAST(LTRIM(RTRIM(customer_id)) AS INT) AS customer_id,
+			LTRIM(RTRIM(invoice_no)) AS invoice_no,
+			CONVERT(DATE, invoice_date , 101) AS invoice_date,
+			UPPER(country) AS country,
+			LTRIM(RTRIM(stock_code)) AS stock_code_category,
+			LTRIM(RTRIM(UPPER(REPLACE(stock_name, '"', '')))) AS stock_name_category,
+			CAST(LTRIM(RTRIM(stock_quantity)) AS INT) as stock_quantity,
+			CAST(stock_unit_price AS FLOAT) AS stock_unit_price
+		FROM bronze.ecommerce
+		-- Exclude rows which have NULL value data in these 2 column
+		WHERE stock_name IS NOT NULL AND customer_id IS NOT NULL),
+
+		-- CTE to add variant_id as some stock_id/stock_name of 1 product consist of multiple stock_unit_price
+		-- E.G. [stock_id = 101] [stock_name: towel] [Price:0.85, 0.49]
+		cte_add_variantid AS(
+		SELECT
+			customer_id,
+			invoice_no,
+			invoice_date,
+			country,
+			stock_code_category,
+			stock_name_category,
+			-- To correct naming same stock_code & price but different naming, here base on DENSE RANK or the NEXT QUERY 
+		
+			DENSE_RANK() OVER ( PARTITION BY stock_code_category ORDER BY stock_unit_price ASC
+			) AS variant_id,
+			stock_quantity,
+			stock_unit_price
+		FROM cte_cleaned_data
+		-- Exclude data value such as Manual input, postage, discount and so on
+		WHERE stock_code_category NOT LIKE '[a-zA-Z]%'),
+
+		-- CTE to add stock_code_variant, stock_name_variant column
+		cte_add_stockid_stockname_variant_data AS(
+		SELECT
+			customer_id,
+			invoice_no,
+			invoice_date,
+			country,
+			stock_code_category,
+			stock_name_category,
+			variant_id,
+			-- Add variant_id to differentiate each variant for code & name due to different pricing
+			stock_code_category +' v' + CAST(variant_id AS NVARCHAR(10)) AS stock_code_variant,
+			stock_name_category +' v' + CAST(variant_id AS NVARCHAR(10)) AS stock_name_variant,
+			stock_quantity,
+			stock_unit_price
+		FROM cte_add_variantid)
+
+		-- Insert data to table silver.ecommerce using CTE: cte_cleaned_order_data
+		INSERT INTO silver.ecommerce
+		SELECT
+			*
+		FROM cte_add_stockid_stockname_variant_data
+
+		SET @end_time = GETDATE()
+		PRINT '>>>>>LOAD DURATION: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' SECONDS';
+        PRINT '>>>>>LOAD COMPLETE silver.ecommerce';
+        PRINT '>>>>>-------------';
+		
+		PRINT '===============================';
+		PRINT 'LOADING silver.dim_customer';
+		PRINT '===============================';
+
+		-- Loading silver.dim_customers
+		SET @start_time = GETDATE();
+
+		PRINT '>>>>>TRUNCATING TABLE: silver.dim_customer';
+		TRUNCATE TABLE silver.dim_customers;
+		PRINT '>>>>>INSERTING TO TABLE: silver.dim_customer';
+
+		-- CTE to find out the latest row record base on invoice_date
+		WITH cte_latest_record AS (
+			SELECT 
+			customer_id,
+			country,
+			ROW_NUMBER() OVER (PARTITION BY customer_id
+			-- To label latest record as value: flag = 1
+			ORDER BY invoice_date DESC
+			) AS flag
+		FROM silver.ecommerce)
+
+		-- Insert data to table using CTE: cte_latest_record
+		INSERT INTO silver.dim_customers
+		SELECT
+			customer_id,
+			country
+		FROM cte_latest_record
+		-- flag =1 to only insert the latest row record
+		WHERE flag =1
+
 		SET @end_time = GETDATE();
 		PRINT '>>>>>LOAD DURATION: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' SECONDS';
-        PRINT '>>>>>LOAD COMPLETE'
+        PRINT '>>>>>LOAD COMPLETE silver.dim_customers';
+        PRINT '>>>>>-------------';
+
+		PRINT '===============================';
+		PRINT 'LOADING silver.dim_stocks';
+		PRINT '===============================';
+
+		-- Loading silver.dim_stocks
+		SET @start_time = GETDATE();
+		PRINT '>>>>>TRUNCATING TABLE: silver.dim_stocks';
+		TRUNCATE TABLE silver.dim_stocks;
+		PRINT '>>>>>INSERTING TO TABLE: silver.dim_stocks';
+
+		--CTE to filter and keep only 1 record for stock of each variant
+		WITH cte_filter_stocks AS(
+		SELECT
+			stock_code_category,
+			stock_name_category,
+			variant_id,
+			stock_code_variant,
+			stock_name_variant,
+			stock_unit_price,
+			-- Only keep 1 record for each stock of each variant
+			ROW_NUMBER() OVER ( PARTITION BY stock_name_variant ORDER BY stock_unit_price ASC
+			) AS flag
+		FROM silver.ecommerce)
+
+		-- Insert data to table using CTE: cte_filter_stocks
+		INSERT INTO silver.dim_stocks
+		SELECT
+			stock_code_category,
+			stock_name_category,
+			variant_id,
+			stock_code_variant,
+			stock_name_variant,
+			stock_unit_price
+		FROM cte_filter_stocks
+		WHERE flag = 1
+
+		SET @end_time = GETDATE();
+		PRINT '>>>>>LOAD DURATION: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' SECONDS';
+        PRINT '>>>>>LOAD COMPLETE silver.dim_stocks';
+        PRINT '>>>>>-------------';
+
+
+		PRINT '===============================';
+		PRINT 'LOADING silver.fact_sales';
+		PRINT '==============================='
+
+		-- Loading silver.fact_sales
+		SET @start_time = GETDATE();
+		PRINT '>>>>>TRUNCATING TABLE: silver.fact_sales';
+		TRUNCATE TABLE silver.fact_sales;
+		PRINT '>>>>>INSERTING TO TABLE: silver.fact_sales';
+
+		/*SELECT
+		customer_id,
+		invoice_no,
+		invoice_date,
+		stock_code,
+		stock_quantity,
+		stock_unit_price
+		FROM silver.ecommerce*/
+
+		SET @batch_end_time = GETDATE();
+		PRINT 'BATCH LOAD DURATON ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' SECONDS';
+		PRINT '>>>>>LOAD COMPLETE SILVER LAYER';
         PRINT '>>>>>-------------';
 	END TRY
 
 	BEGIN CATCH
-			PRINT '=========================================='
-			PRINT 'ERROR OCCURED DURING LOADING SILVER LAYER'
-			PRINT 'ERROR MESSAGE' + ERROR_MESSAGE();
-			PRINT 'ERROR NUMBER' + CAST (ERROR_NUMBER() AS NVARCHAR);
-			PRINT 'ERROR STATE' + CAST (ERROR_STATE() AS NVARCHAR);
-			PRINT '=========================================='
+		PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING SILVER LAYER'
+		PRINT 'ERROR MESSAGE ' + ERROR_MESSAGE();
+		PRINT 'ERROR NUMBER ' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'ERROR STATE ' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
 	END CATCH
 END
